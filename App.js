e5b990cb3084765a8e1892815babf8bbd7b90efa
Yamil Leotta <108009904+YamilLeotta@users.noreply.github.com>
2024-10-05 01:51:38 -0300

    SafeAreaContext

--- App.js
+++ App.js
@@ -1,39 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Image,
-} from "react-native";
-import { getLastestGames } from "./lib/metacritic";
-import Constants from "expo-constants";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Main from "./components/Main";
 
 export default function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {getLastestGames().then(games => setGames(games))}, []);
-
   return (
+    <SafeAreaProvider>
       <View style={styles.container}>
         <StatusBar style="light" />
-
-          <ScrollView>
-            {games.map(game => (
-              <View key={game.slug} style={styles.card}>
-                <Image
-                  source={{uri: game.image}}
-                  style={styles.image}
-                />
-                <Text style={styles.title}>{game.title}</Text>
-                <Text style={styles.score}>{game.score}</Text>
-                <Text style={styles.description}>{game.description}</Text>
-              </View>
-              ))}
-          </ScrollView>
-
+        <Main />
       </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -43,30 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: Constants.statusBarHeight
   },
-  card: {
-    marginBottom: 42
-  },
-  image: {
-    width: 107,
-    height: 147,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 10,
-    color: "white"
-  },
-  description: {
-    fontSize: 16,
-    color: "#eee"
-  },
-  score: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "green",
-    marginBottom: 10
-  }
 });
